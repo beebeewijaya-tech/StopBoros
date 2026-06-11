3cefcfe827b3124ca3f832785d227e1e7006057d
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-12 06:53:36 +0700

    feat: implementing main screen slicing
    -- What
    
    add several UI slicing regarding the stopboros by putting several important information
    
    * add the app category / expense
    * add the app progress
    * add the app card

---
+++ StopBoros/Components/AppExpense.swift
@@ -0,0 +1,98 @@
+//
+//  AppCategory.swift
+//  StopBoros
+//
+//  Created by Bee Wijaya on 12/06/26.
+//
+
+import SwiftUI
+
+enum ExpenseCategory {
+    case food, transport, shopping
+    
+    var background: Color {
+        switch self {
+        case .food:
+            return Color("GreenLight")
+        case .transport:
+            return Color("Transport")
+        case .shopping:
+            return Color("Shopping")
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .food:
+            return "fork.knife"
+        case .transport:
+            return "car.fill"
+        case .shopping:
+            return "bag.fill"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .food:
+            return "Food"
+        case .transport:
+            return "Transport"
+        case .shopping:
+            return "Shopping"
+        }
+    }
+}
+
+struct Expense: Identifiable {
+    var id: UUID = .init()
+    var expense: String
+    var category: ExpenseCategory
+    var created: Date
+    var amount: Double
+}
+
+struct AppExpense: View {
+    var expense: Expense
+    
+    var formatedDate: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, h:mm a"
+        return formatter.string(from: expense.created)
+    }
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Image(systemName: expense.category.icon)
+                    .foregroundStyle(expense.category.background)
+                    .font(.body)
+            }
+            .padding()
+            .frame(width: 40, height: 40)
+            .background(expense.category.background.opacity(0.4))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding(.trailing, 6)
+            
+            
+            VStack(alignment: .leading) {
+                Text(expense.expense)
+                    .foregroundStyle(.white)
+                    .bold()
+                    .font(.title3)
+                
+                Text("\(expense.category.title) - \(formatedDate)")
+                    .foregroundStyle(Color("GrayAsh"))
+                    .font(.caption)
+            }
+            
+            Spacer()
+            
+            Text("- Rp. \(expense.amount.formatted())")
+                .foregroundStyle(Color("Alert"))
+                .bold()
+                .font(.callout)
+        }
+        .padding(.bottom, 8)
+    }
+}
--- StopBoros/Screens/MainScreen.swift
+++ StopBoros/Screens/MainScreen.swift
@@ -7,9 +7,104 @@
 
 import SwiftUI
 
+
+
+
 struct MainScreen: View {
+    @State private var progress: CGFloat = 1/10
+    private var expenses: [Expense] = [
+        Expense(expense: "Grab", category: .transport, created: .now, amount: 15000),
+        Expense(expense: "Lunch", category: .food, created: .distantPast, amount: 25000),
+        Expense(expense: "Kopi Tuku", category: .shopping, created: .distantFuture, amount: 18000),
+    ]
+    
     var body: some View {
-        Text("Main Screen")
+        ZStack {
+            Color("AppBackground")
+                .ignoresSafeArea(.all)
+            
+            
+            VStack {
+                HStack(alignment: .center) {
+                    VStack(alignment: .leading) {
+                        Text("Thursday, June 11")
+                            .font(.caption)
+                            .foregroundStyle(Color("GrayAsh"))
+                        
+                        Text("Hey Bee 👋")
+                            .foregroundStyle(.white)
+                            .bold()
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "bell")
+                            .foregroundStyle(.white)
+                    }
+                    .padding(12)
+                    .glassEffect(.clear)
+                }
+                .padding(.bottom, 20)
+                
+                AppCard(label: "Spent Today", value: "Rp 100.000", style: .secondary, size: .large, progress: progress) {
+                    HStack {
+                        AppCard(label: "This Week", value: "Rp 100k", style: .primary, size: .medium, progress: nil) {
+                            
+                        }
+                        AppCard(label: "This Month", value: "Rp 1.2m", style: .primary, size: .medium, progress: nil) {
+                            
+                        }
+                    }
+                }
+                .padding(.bottom, 20)
+                
+                
+                VStack {
+                    Text("You've spent 80% of your food budget — and it's only Wednesday.")
+                        .foregroundStyle(Color("Expense"))
+                        .bold()
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color("Alert").opacity(0.15))
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(style: StrokeStyle(lineWidth: 2))
+                        .fill(Color("Alert"))
+                }
+                .padding(.bottom, 24)
+                
+                
+                VStack {
+                    HStack {
+                        Text("Today's Transactions")
+                            .font(.body)
+                            .foregroundStyle(.white)
+                            .bold()
+                        Spacer()
+                        Text("See all")
+                            .font(.body)
+                            .foregroundStyle(Color("GrayAsh"))
+                    }
+                    .padding(.bottom, 12)
+                    
+                    LazyVStack {
+                        ForEach(expenses.indices, id: \.self) { idx in
+                            AppExpense(expense: expenses[idx])
+                        }
+                    }
+                }
+                
+                
+                Spacer()
+            }
+            .padding(34)
+        }
     }
 }
 
