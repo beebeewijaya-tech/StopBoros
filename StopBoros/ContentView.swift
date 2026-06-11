7a7c778e0cffd92f2dd49c033d0c4602cf240572
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-11 21:05:53 +0700

    Initial Commit

---
+++ StopBoros/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StopBoros
+//
+//  Created by Bee Wijaya on 11/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StopBoros/StopBorosApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopBorosApp.swift
+//  StopBoros
+//
+//  Created by Bee Wijaya on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct StopBorosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
