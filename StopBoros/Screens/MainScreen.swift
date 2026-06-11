0e33bb1e67f20204e6dfec83f64a67ae3b8bada1
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-06-11 21:21:02 +0700

    initial: setup the project of stop-boros
    -- What
    
    setting app the color pallete and app icon and directory

---
+++ StopBoros/Screens/MainScreen.swift
@@ -0,0 +1,19 @@
+//
+//  MainScreen.swift
+//  StopBoros
+//
+//  Created by Bee Wijaya on 11/06/26.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text("Main Screen")
+    }
+}
+
+
+#Preview {
+    MainScreen()
+}
